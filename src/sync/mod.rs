b4a5059a738c4bcd43f173dffad9c524598b846e
Eugene <usachblog@gmail.com>
2024-08-22 23:34:03 +0300

    Add Once

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -2,9 +2,10 @@ pub mod local;
 pub mod naive_mutex;
 pub mod wait_group;
 pub mod naive_rw_lock;
-mod once;
+pub mod once;
 
 pub use local::*;
 pub use naive_mutex::*;
 pub use wait_group::*;
 pub use naive_rw_lock::*;
+pub use once::*;
