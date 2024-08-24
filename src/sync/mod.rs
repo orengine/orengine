2bdb0c442aa57c780fe85b0ebb9794317a6e39a1
Eugene <usachblog@gmail.com>
2024-08-24 14:38:52 +0300

    Add LazyLock

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,11 +1,12 @@
-pub mod local;
-pub mod naive_mutex;
-pub mod wait_group;
-pub mod naive_rw_lock;
-pub mod once;
-
 pub use local::*;
 pub use naive_mutex::*;
-pub use wait_group::*;
 pub use naive_rw_lock::*;
 pub use once::*;
+pub use wait_group::*;
+
+mod lazy_lock;
+pub mod local;
+pub mod naive_mutex;
+pub mod naive_rw_lock;
+pub mod once;
+pub mod wait_group;
