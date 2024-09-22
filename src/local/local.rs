a2871ebdf72aa5d9aee97c2362c9a80b62566191
Eugene <usachblog@gmail.com>
2024-09-22 22:49:20 +0300

    Add docs for local

--- src/local/local.rs
+++ src/local/local.rs
@@ -1,42 +1,114 @@
-#![allow(dead_code)]
 // TODO docs
 
 use std::fmt::Debug;
 use crate::utils::Ptr;
 
+/// A private structure used within Local to hold a reference counter
+/// and the actual data.
+///
+/// It ensures that the reference counting mechanism is tightly coupled with the data being stored.
 struct Inner<T> {
+    /// A simple reference counter, incremented when new references are created and decremented
+    /// when references are dropped.
     counter: usize,
+    /// The user-defined data that the [`Local`] struct manages.
     data: T
 }
 
+/// [`Local`] is a custom smart pointer-like structure designed to manage `T` in a single-threaded,
+/// non-shared memory environment.
+/// This design is aligned with the shared-nothing concurrency model, which avoids shared memory
+/// between threads to reduce the complexity of synchronization.
+///
+/// In this model, each thread has its own isolated memory,
+/// ensuring that data is not concurrently accessed by multiple threads.
+/// This provides simpler concurrency and eliminates
+/// the need for complex locking or atomic operations.
+///
+/// [`Local`] is not [`Send`] by design, meaning it cannot be transferred across threads,
+/// and it should only be used in single-threaded (local) contexts.
+///
+/// This makes [`Local`] ideal for managing thread-local data,
+/// where the overhead of atomic reference counting or synchronization is unnecessary.
+/// Instead, you get a simpler and more efficient implementation by using a raw pointer
+/// with manual reference counting.
+///
+/// # Shared-Nothing and spawn_local
+///
+/// In Rust, when working with local, task-specific data, you may want to leverage
+/// shared-nothing concurrency by isolating data to specific threads.
+/// Using the [`Local`] struct with [`spawn_local`](crate::runtime::Executor::spawn_local)
+/// (which ensures that a task runs in a local thread) allows you to maintain this isolation.
+/// Since Local cannot be transferred to another thread, it enforces the shared-nothing principle,
+/// where each every thread holds and manages its own data independently.
+///
+/// # Example
+///
+/// ```rust
+/// use orengine::local::Local;
+/// use orengine::Executor;
+/// use orengine::local_executor;
+///
+/// fn main() {
+///     Executor::init().run_with_local_future(async {
+///         let local_data = Local::new(42);
+///
+///         // Get immutable access
+///         let value = local_data.get();
+///         println!("The value is: {}", value);
+///
+///         // Get mutable access
+///         let mut_value = local_data.get_mut();
+///         *mut_value += 1;
+///
+///         println!("Updated value: {}", local_data.get());
+///
+///         // Cloning (increments the internal counter)
+///         let cloned_data = local_data.clone();
+///         local_executor().spawn_local(async move {
+///             // Work here with the cloned data.
+///             assert_eq!(*cloned_data.get(), 42);
+///         });
+///     });
+/// }
+/// ```
 pub struct Local<T> {
     inner: Ptr<Inner<T>>
 }
 
 impl<T> Local<T> {
+    /// Creates a new [`Local`] instance, initializing it with the provided data.
     pub fn new(data: T) -> Self {
         Local {
             inner: Ptr::new(Inner { data, counter: 1 })
         }
     }
-    
+
+    /// Increments the internal reference counter.
+    /// This is called when a new reference to the [`Local`] is created
+    /// (e.g., when `clone()` is called).
     #[inline(always)]
     fn inc_counter(&self) {
         unsafe { self.inner.as_mut().counter += 1; }
     }
-    
+
+    /// Decrements the internal reference counter. This is called when a reference is dropped
+    /// (e.g., when the [`Local`] is dropped).
+    /// If the reference count reaches zero, the data is cleaned up.
     #[inline(always)]
     fn dec_counter(&self) -> usize {
         let reference = unsafe { self.inner.as_mut() };
         reference.counter -= 1;
         reference.counter
     }
-    
+
+    /// Returns an immutable reference to the inner data.
     #[inline(always)]
     pub fn get<'local>(&self) -> &'local T {
         unsafe { &self.inner.as_ref().data }
     }
-    
+
+    /// Returns a mutable reference to the inner data.
     #[inline(always)]
     pub fn get_mut<'local>(&self) -> &'local mut T {
         unsafe { &mut self.inner.as_mut().data }
