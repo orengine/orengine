d75df73ea780d5e129f656f01754e474fe283307
Eugene <usachblog@gmail.com>
2024-10-07 18:52:00 +0300

    Add docs for spin_lock

--- src/utils/spin_lock.rs
+++ src/utils/spin_lock.rs
@@ -1,5 +1,10 @@
+//! This module provides an __blocking__ mutex (e.g. [`std::sync::Mutex`]) type [`SpinLock`].
+//! It allows for __blocking__ locking and unlocking, and provides
+//! ownership-based locking through [`SpinLockGuard`].
+//!
+//! It locks the current __thread__ until it acquires the lock. Use it only for short locks and
+//! only if it is not possible to use asynchronous locking.
 use crossbeam::utils::{Backoff, CachePadded};
-// TODO docs
 use std::cell::UnsafeCell;
 use std::mem;
 use std::ops::{Deref, DerefMut};
@@ -7,37 +12,70 @@ use std::sync::atomic;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 
+/// An RAII implementation of a "scoped lock" of a mutex. When this structure is
+/// dropped (falls out of scope), the lock will be unlocked.
+///
+/// The data protected by the mutex can be accessed through this guard via its
+/// [`Deref`](Deref) and [`DerefMut`] implementations.
+///
+/// This structure is created by the [`lock`](SpinLock::lock)
+/// and [`try_lock`](SpinLock::try_lock) methods on [`SpinLock`].
 pub struct SpinLockGuard<'spin_lock, T> {
     spin_lock: &'spin_lock SpinLock<T>,
 }
 
 impl<'spin_lock, T> SpinLockGuard<'spin_lock, T> {
+    /// Creates a new [`SpinLockGuard`].
     #[inline(always)]
     pub(crate) fn new(spin_lock: &'spin_lock SpinLock<T>) -> Self {
         Self { spin_lock }
     }
 
+    /// Returns a reference to the original [`SpinLock`].
     #[inline(always)]
     pub fn spin_lock(&self) -> &SpinLock<T> {
         &self.spin_lock
     }
 
-    #[inline(always)]
-    /// Unlocks the spin_lock. Calling `guard.unlock()` is equivalent to calling `drop(guard)`.
-    /// This was done to improve readability.
+    /// Unlocks the [`spin_lock`](SpinLock). Calling `guard.unlock()` is equivalent to
+    /// calling `drop(guard)`. This was done to improve readability.
     ///
     /// # Attention
     ///
     /// Even if you doesn't call `guard.unlock()`,
-    /// the spin_lock will be unlocked after the `guard` is dropped.
+    /// the [`spin_lock`](SpinLock) will be unlocked after the `guard` is dropped.
+    #[inline(always)]
     pub fn unlock(self) {}
 
+    /// Returns a reference to the original [`SpinLock`].
+    ///
+    /// The lock will never be unlocked.
+    ///
+    /// # Safety
+    ///
+    /// The mutex is unlocked by calling [`SpinLock::unlock`] later.
     #[inline(always)]
     pub unsafe fn leak(self) -> *const CachePadded<AtomicBool> {
         let atomic_ptr = &self.spin_lock.is_locked;
         mem::forget(self);
 
         atomic_ptr
+    }
+
+    /// Returns a reference to the [`CachePadded<AtomicBool>`]
+    /// associated with the original [`SpinLock`] to
+    /// use [`Executor::release_atomic_bool`](crate::Executor::release_atomic_bool).
+    ///
+    /// # Safety
+    ///
+    /// The mutex is unlocked by calling [`SpinLock::unlock`] later or
+    /// [`Executor::release_atomic_bool`](crate::Executor::release_atomic_bool).
+    #[inline(always)]
+    pub unsafe fn leak_to_atomic(self) -> &'spin_lock CachePadded<AtomicBool> {
+        let static_spin_lock = unsafe { mem::transmute(&self.spin_lock.is_locked) };
+        mem::forget(self);
+
+        static_spin_lock
     }
 }
 
@@ -61,12 +99,27 @@ impl<'spin_lock, T> Drop for SpinLockGuard<'spin_lock, T> {
     }
 }
 
+/// A mutual exclusion primitive useful for protecting shared data.
+///
+/// This mutex will block __thread__ waiting for the lock to become available. The
+/// mutex can be created via a [`new`](SpinLock::new) constructor. Each mutex has a type parameter
+/// which represents the data that it is protecting. The data can be accessed
+/// through the RAII guards returned from [`lock`](SpinLock::lock) and
+/// [`try_lock`](SpinLock::try_lock),
+/// which guarantees that the data is only ever accessed when the mutex is locked, or
+/// with an unsafe method [`get_locked`](SpinLock::get_locked).
+///
+/// # The difference between `SpinLock` and other mutexes
+///
+/// It locks the current __thread__ until it acquires the lock. Use it only for short locks and
+/// only if it is not possible to use asynchronous locking.
 pub struct SpinLock<T> {
     is_locked: CachePadded<AtomicBool>,
     value: UnsafeCell<T>,
 }
 
 impl<T> SpinLock<T> {
+    /// Creates a new [`SpinLock`].
     #[inline(always)]
     pub const fn new(value: T) -> SpinLock<T> {
         SpinLock {
@@ -75,6 +128,7 @@ impl<T> SpinLock<T> {
         }
     }
 
+    /// Blocks the current __thread__ until it acquires the lock.
     #[inline(always)]
     pub fn lock(&self) -> SpinLockGuard<T> {
         let backoff = Backoff::new();
@@ -87,6 +141,8 @@ impl<T> SpinLock<T> {
         }
     }
 
+    /// If `SpinLock` is unlocked, returns [`SpinLockGuard`] that allows access to the inner value,
+    /// otherwise returns [`None`].
     #[inline(always)]
     pub fn try_lock(&self) -> Option<SpinLockGuard<T>> {
         if self
@@ -100,21 +156,36 @@ impl<T> SpinLock<T> {
         }
     }
 
+    /// Returns a reference to the underlying data. It is safe because it uses `&mut self`.
     #[inline(always)]
     pub fn get_mut(&mut self) -> &mut T {
         self.value.get_mut()
     }
 
-    #[inline(always)]
-    pub unsafe fn get_locked(&self) -> &mut T {
-        debug_assert!(self.is_locked.load(Acquire));
-        unsafe { &mut *self.value.get() }
-    }
-
+    /// Unlocks the mutex.
+    ///
+    /// # Safety
+    ///
+    /// - The `SpinLock` must be locked.
+    ///
+    /// - And no threads has an ownership of this `SpinLock`.
     #[inline(always)]
     pub unsafe fn unlock(&self) {
         debug_assert!(self.is_locked.load(Acquire));
         self.is_locked.store(false, Release);
+    }
+
+    /// Returns a reference to the inner value.
+    ///
+    /// # Safety
+    ///
+    /// - The `SpinLock` must be locked.
+    ///
+    /// - And only current task has an ownership of this `SpinLock`.
+    #[inline(always)]
+    pub unsafe fn get_locked(&self) -> &mut T {
+        debug_assert!(self.is_locked.load(Acquire));
+        unsafe { &mut *self.value.get() }
     }
 }
 
