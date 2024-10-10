e0dc9e23c16596e5226cc998cfa8336f7c7cfcb3
Eugene <usachblog@gmail.com>
2024-10-10 22:16:25 +0300

    Add examples

---
+++ examples/echo-server/src/main.rs
@@ -0,0 +1,31 @@
+use orengine::buf::full_buffer;
+use orengine::net::{Listener, Stream, TcpListener};
+use orengine::{local_executor, run_on_all_cores};
+
+async fn handle_stream<S: Stream>(mut stream: S) {
+    loop {
+        stream.poll_recv().await.expect("poll failed");
+
+        let mut buf = full_buffer();
+        let n = stream.recv(&mut buf).await.expect("recv failed");
+        if n == 0 {
+            break;
+        }
+
+        stream.send_all(&buf[..n]).await.expect("send failed");
+    }
+}
+
+async fn run_listener<L: Listener>() {
+    let mut listener = L::bind("127.0.0.1:8080").await.expect("bind failed");
+
+    while let Ok((stream, _addr)) = listener.accept().await {
+        local_executor().spawn_local(async move {
+            handle_stream(stream).await;
+        });
+    }
+}
+
+fn main() {
+    run_on_all_cores(run_listener::<TcpListener>);
+}
---
+++ examples/fs/src/main.rs
@@ -0,0 +1,46 @@
+use orengine::buf::buffer;
+use orengine::fs::{DirBuilder, File, OpenOptions};
+use orengine::io::{AsyncRead, AsyncWrite};
+use orengine::{asyncify, Executor};
+use std::os::unix::fs::DirBuilderExt;
+
+fn main() {
+    Executor::init()
+        .run_and_block_on_local(async {
+            let _ = DirBuilder::new()
+                .mode(0o777)
+                .recursive(true)
+                .create("./foo/bar")
+                .await;
+            let open_options = OpenOptions::new()
+                .create(true)
+                .read(true)
+                .write(true)
+                .mode(0o777)
+                .truncate(true);
+            let mut file = File::open("./foo/bar/test.txt", &open_options)
+                .await
+                .unwrap();
+
+            file.write_all(b"Hello, world!").await.unwrap(); // non-positioned write
+
+            let mut buf = buffer();
+            buf.set_len(5);
+            file.pread_exact(&mut buf, 7).await.unwrap(); // positioned read
+
+            assert_eq!(buf, b"world");
+
+            // Now Orengine doesn't support read_dir, but you can use `std::fs::read_dir`
+            // in async runtime with `asyncify`.
+            asyncify!(|| {
+                let res = std::fs::read_dir("./foo/bar").unwrap();
+                for entry in res {
+                    let entry = entry.unwrap();
+                    let path = entry.path();
+                    println!("{:?}", path); // "./foo/bar/test.txt"
+                }
+            })
+            .await;
+        })
+        .expect("Execution failed");
+}
