8bc5074c83beec5e0d75192ad5932d1a649af160
Eugene <usachblog@gmail.com>
2024-10-12 15:53:41 +0300

    Created a synonyms with Fd for Handles

--- src/io/sys/mod.rs
+++ src/io/sys/mod.rs
@@ -1,8 +1,14 @@
 //! Helper for work with the system.
+#[cfg(unix)]
 pub(crate) mod unix;
+#[cfg(windows)]
+pub(crate) mod windows;
 
+pub(crate) use io_uring::types::OpenHow;
+#[cfg(unix)]
 pub(crate) use unix::fd::*;
 pub(crate) use unix::os_message_header::*;
-pub(crate) use io_uring::types::OpenHow;
-pub(crate) use unix::IoUringWorker as WorkerSys;
 pub(crate) use unix::os_path as OsPath;
+pub(crate) use unix::IoUringWorker as WorkerSys;
+#[cfg(windows)]
+pub(crate) use windows::fd::*;
